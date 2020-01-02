cb1dc083c0eea7cad63f6abde095f1b4f3fe6ecb
Pau <pabtorca@gmail.com>
2020-01-02 18:45:42 +0100

    Adjust the rest of helper controls
    Add a tab item to show the calculations to support the conversion from local to global coordinate system

---
+++ 3DPoC/POC3D/POC3D/Controls/Matrix/Controls/Helpers/Vector1Control.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace POC3D.Controls.Matrix.Controls.Helpers
+{
+    /// <summary>
+    ///     Interaction logic for Vector1Control.xaml
+    /// </summary>
+    public partial class Vector1Control : UserControl
+    {
+        public static readonly DependencyProperty V1Property = DependencyProperty.Register(
+            nameof(V1), typeof(string), typeof(Vector1Control),
+            new PropertyMetadata(string.Empty, PropertyChangedCallback));
+
+
+        public Vector1Control()
+        {
+            InitializeComponent();
+        }
+
+        public string V1
+        {
+            get => Position1.Text;
+            set => SetValue(V1Property, value);
+        }
+
+        private static void PropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as Vector1Control;
+
+            if (control == null)
+            {
+                return;
+            }
+
+            var value = e.NewValue as string;
+            control.Position1.Text = value;
+        }
+    }
+}

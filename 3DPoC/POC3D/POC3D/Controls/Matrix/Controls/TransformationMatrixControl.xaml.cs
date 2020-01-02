f8fbe69630b8b9d052cd7029b7c4b44eda327ca4
Pau <pabtorca@gmail.com>
2020-01-02 19:14:37 +0100

    Transformation matrix control added

---
+++ 3DPoC/POC3D/POC3D/Controls/Matrix/Controls/TransformationMatrixControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POC3D.Controls.Matrix.Controls
+{
+    /// <summary>
+    /// Interaction logic for TransformationMatrixControl.xaml
+    /// </summary>
+    public partial class TransformationMatrixControl : UserControl
+    {
+        public TransformationMatrixControl()
+        {
+            InitializeComponent();
+        }
+    }
+}

041f7d2da1117511df9689202fc8b836cab234e0
Pau <pabtorca@gmail.com>
2020-03-21 13:03:01 +0100

    Add tests for nodeviewmodel

---
+++ 3DPoC/POC3D/POC3D.ViewModel.Tests/Helper/PropertyChangedEventChecker.cs
@@ -0,0 +1,23 @@
+﻿using POC3D.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POC3D.ViewModel.Tests.Helper
+{
+    public class PropertyChangedEventChecker
+    {
+        private Observable _observable;
+
+        public PropertyChangedEventChecker(Observable observable)
+        {
+            _observable = observable;
+
+            _observable.PropertyChanged += (_ , e) => PropertiesRaised.Add(e.PropertyName);
+        }
+
+        public List<string> PropertiesRaised { get; } = new List<string>();
+    }
+}
